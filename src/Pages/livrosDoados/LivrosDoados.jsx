d5286d47436b7ab01a89935258d5e2d4ce724064
cleannelima <cleannelima10@gmail.com>
2024-12-07 22:13:27 -0300

    adição de livros doados e estilização

--- src/Pages/livrosDoados/LivrosDoados.jsx
+++ src/Pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,22 @@
-export default function LivrosDoados(){
-    return(
-        <h2>Eu sou o Livros Doados</h2>
+import s from "../../Pages/livrosDoados/livrosdoados.module.scss"
+import protagonista from "../../assets/protagonista.png"
+
+export default function LivrosDoados() {
+    return (
+        <main>
+            <h2 className={s.title}>Livros Doados</h2>
+            <section className={s.card}>
+                <img
+                    src={protagonista}
+                    alt="Imagem de um ícone de um livro, nomeado como protagonista."
+                    className={s.cardImage}
+                />
+                <section className={s.cardContent}>
+                    <p className={s.tituloDoLivro}>O protagonista</p>
+                    <p className={s.autor}>Susanne Andrade</p>
+                    <p className={s.genero}>Ficção</p>
+                </section>
+            </section>
+        </main>
     )
 }
